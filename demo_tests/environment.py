39228fd22155a55d35fd6ee41761230bd1587045
Katerina <katerina@ati.su>
2014-04-15 18:20:08 +0400

    new test

--- demo_tests/environment.py
+++ demo_tests/environment.py
@@ -5,22 +5,20 @@ from selenium import webdriver
 import sys
 import locale
 
+
+# wild and dirty hack for windows platform for setting valid locale encoding
+if sys.platform in ['win32', 'cygwin']:
+    def default_enc(*args, **kwargs):
+        return "utf8"
+    locale.getpreferredencoding = default_enc
+
+    
 def before_all(context):
     context.browser = webdriver.Firefox()
-    
-    
+       
 def after_all(context):
     try:
         context.browser.close()
         context.browser.quit()
     except Exception:
         pass
-
-
-# wild and dirty hack for windows platform for setting valid locale encoding
-if sys.platform in ['win32', 'cygwin']:
-    # override builtin method which returns default encoding used for opening
-    # files
-    def default_enc(*args, **kwargs):
-        return "utf8"
-    locale.getpreferredencoding = default_enc
---
+++ demo_tests/test.feature
@@ -0,0 +1,9 @@
+#language: ru
+
+Функционал: сайт SQA Days
+    Сценарий: Тестируем переход на английскую версию сайта
+        Если зайти на сайт http://sqadays.com/
+        И нажать на ссылку "En"
+        Тогда url страницы станет http://sqadays.com/en/index
+        И на странице будет текст "XV International Conference of Software Quality Assurance"
+        
